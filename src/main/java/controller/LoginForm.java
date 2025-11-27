9eb9ba716eb18ef80c15cd170c7c0d359d98947f
itzisurii <itzIsurika08@gmail.com>
2025-11-27 23:46:51 +0530

    implement login button action with credential validation and scene navigation

---
+++ src/main/java/controller/LoginForm.java
@@ -0,0 +1,50 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginForm {
+
+    Stage stage = new Stage();
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        if(username.equals("isu")){
+            if(password.equals("1234")){
+                try {
+                    stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/customer_management_controller.fxml"))));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                stage.show();
+                txtUsername.clear();
+                txtPassword.clear();
+            }
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText("Invalid Credentials");
+            alert.setContentText("Username or Password is incorrect.");
+            alert.show();
+        }
+    }
+
+}
